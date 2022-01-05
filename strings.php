cf77f402937acec8d8f5364923bcf7033e2e0251
Lucas tecluu <57456310+2610n@users.noreply.github.com>
2022-01-05 16:58:05 -0300

    tipo de strings

---
+++ strings.php
@@ -0,0 +1,20 @@
+
+<?php
+
+$nome="lucas victor";
+
+
+echo ucfirst($nome); //define a primeira letra de um paragrafo em maiusculo
+exit;
+echo ucwords($nome); // ele converte as primeiras letras de cada palavra em maiuscula 
+exit;
+echo str_replace('u','0',$nome);// funçao que altera alguma letra  no valor da variavel 
+exit;
+echo strtolower($nome);//converte em letras minusculas 
+exit;
+echo strtoupper($nome);//converte  em letras maiusculas
+
+
+
+
+?>
