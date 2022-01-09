9b07d930a312f06251438d0a64b05af27af10b62
Lucas tecluu <57456310+2610n@users.noreply.github.com>
2022-01-09 19:14:50 -0300

    for

---
+++ for.php
@@ -0,0 +1,16 @@
+<?php
+
+for($cont=1;$cont<=10;$cont++){
+
+    if($cont <=5){
+     continue;//este comando  cria uma pulo  de acordo com o parametro if 
+    }
+    if($cont ==7)break;//ele vai para a contagem 
+
+    echo "n°=$cont<br>";
+
+}
+
+
+
+?>
